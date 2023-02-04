79e079cf01c5bd663d897003bd92037dd63e6ef8
Donkay20 <supersett@hotmail.com>
2023-02-04 01:54:50 -0500

    v0.14
    Updated dressup screen: bonus text removed and accessories were added. Clothing categories were moved around.

--- Assets/Scripts/ClothingSelection.cs
+++ Assets/Scripts/ClothingSelection.cs
@@ -7,68 +7,70 @@ public class ClothingSelection : MonoBehaviour
 {
     // Start is called before the first frame update
     public static ClothingSelection Instance;
-    public TMPro.TextMeshPro bonusHairText;
-    public TMPro.TextMeshPro bonusTopText;
-    public TMPro.TextMeshPro bonusBottomText;
-    public TMPro.TextMeshPro bonusShoeText;
+//  public TMPro.TextMeshPro bonusHairText;
+//  public TMPro.TextMeshPro bonusTopText;
+//  public TMPro.TextMeshPro bonusBottomText;
+//  public TMPro.TextMeshPro bonusShoeText;
     public TMPro.TextMeshPro startText;
 //  public TMPro.TextMeshPro backText;
 
-    public KeyCode inputUp; public KeyCode inputUpAlt;
-    public KeyCode inputDown; public KeyCode inputDownAlt;
-    public KeyCode inputRight; public KeyCode inputRightAlt;
-    public KeyCode inputLeft; public KeyCode inputLeftAlt;
+    public KeyCode inputUp;
+    public KeyCode inputDown;
+    public KeyCode inputRight;
+    public KeyCode inputLeft;
 
     private int selectedCategory;
     private int selectedClothingHair;
+    private int selectedClothingAccessory;
+    private int selectedClothingShoe;
     private int selectedClothingTop;
     private int selectedClothingBottom;
-    private int selectedClothingShoe;
 
     public GameObject hairBox;
+    public GameObject accessoryBox;
+    public GameObject shoeBox;
     public GameObject topBox;
     public GameObject bottomBox;
-    public GameObject shoeBox;
     public GameObject categoryBox;
 
     void Start()
     {
         Instance = this;
-        selectedCategory = 1;
-        selectedClothingHair = 1;   //category 1
-        selectedClothingTop = 1;    //category 2
-        selectedClothingBottom = 1; //category 3
-        selectedClothingShoe = 1;   //category 4
-        startText.text = "Start!";  //category 5
-    //  backText.text = "Back!";    //category 6
+        selectedCategory = 1;           
+        selectedClothingHair = 1;       //category 1
+        selectedClothingAccessory = 1;  //category 2
+        selectedClothingShoe = 1;       //category 3
+        selectedClothingTop = 1;        //category 4
+        selectedClothingBottom = 1;     //category 5
+        startText.text = "Start!";      //category 6
+    //  backText.text = "Back!";
 
         categoryBox = Instantiate(categoryBox);
         hairBox = Instantiate(hairBox);
+        accessoryBox = Instantiate(accessoryBox);
+        shoeBox = Instantiate(shoeBox);
         topBox = Instantiate(topBox);
         bottomBox = Instantiate(bottomBox);
-        shoeBox = Instantiate(shoeBox);
 
         UpdateCategoryIndicator(selectedCategory);
         UpdateHairIndicator(selectedClothingHair);
+        UpdateAccessoryIndicator(selectedClothingAccessory);
+        UpdateShoeIndicator(selectedClothingShoe);
         UpdateTopIndicator(selectedClothingTop);
         UpdateBottomIndicator(selectedClothingBottom);
-        UpdateShoeIndicator(selectedClothingShoe);
     }
 
     // Update is called once per frame
     void Update()
     {   
 
-        if (selectedCategory != 5) {
+        if (selectedCategory != 6) {
             startText.text = "Start!";
         }
-        //if (selectedCategory != 6) {
-        //  backText.text = "Back!";
-        //}
 
-        if (Input.GetKeyDown(inputUp) || Input.GetKeyDown(inputUpAlt)) {            //go up a category
+        if (Input.GetKeyDown(inputUp)) {            //go up a category
             if (selectedCategory == 1) {
-                selectedCategory = 5;
+                selectedCategory = 6;
             } else {
                 selectedCategory--;
             }
@@ -76,7 +78,7 @@ public class ClothingSelection : MonoBehaviour
         }
 
          if (Input.GetKeyDown(inputDown)) {         //go down a category
-            if (selectedCategory == 5) {
+            if (selectedCategory == 6) {
                 selectedCategory = 1;
             } else {
                 selectedCategory++;
@@ -96,26 +98,16 @@ public class ClothingSelection : MonoBehaviour
             }
 
             if (selectedCategory == 2) {
-                if (selectedClothingTop == 3) {
-                    selectedClothingTop = 1;
+                if (selectedClothingAccessory == 3) {
+                    selectedClothingAccessory = 1;
                 } 
                 else  {
-                    selectedClothingTop++;
+                    selectedClothingAccessory++;
                 }
-                UpdateTopIndicator(selectedClothingTop);
+                UpdateAccessoryIndicator(selectedClothingAccessory);
             }
 
             if (selectedCategory == 3) {
-                if (selectedClothingBottom == 3) {
-                    selectedClothingBottom = 1;
-                } 
-                else  {
-                    selectedClothingBottom++;
-                }
-                UpdateBottomIndicator(selectedClothingBottom);
-            }
-
-            if (selectedCategory == 4) {
                 if (selectedClothingShoe == 3) {
                     selectedClothingShoe = 1;
                 } 
@@ -125,12 +117,32 @@ public class ClothingSelection : MonoBehaviour
                 UpdateShoeIndicator(selectedClothingShoe);
             }
 
+            if (selectedCategory == 4) {
+                if (selectedClothingTop == 3) {
+                    selectedClothingTop = 1;
+                } 
+                else  {
+                    selectedClothingTop++;
+                }
+                UpdateTopIndicator(selectedClothingTop);
+            }
+
             if (selectedCategory == 5) {
+                if (selectedClothingBottom == 3) {
+                    selectedClothingBottom = 1;
+                } 
+                else  {
+                    selectedClothingBottom++;
+                }
+                UpdateBottomIndicator(selectedClothingBottom);
+            }
+
+            if (selectedCategory == 6) {
                 SceneManager.LoadScene("RhythmGame");
             }
         }
 
-         if (Input.GetKeyDown(inputLeft)) {         //go left within the category. if at the return button, go back to ???
+         if (Input.GetKeyDown(inputLeft)) {         //go left within the category.
             if (selectedCategory == 1) {
                 if (selectedClothingHair == 1) {
                     selectedClothingHair = 3;
@@ -141,6 +153,24 @@ public class ClothingSelection : MonoBehaviour
                 UpdateHairIndicator(selectedClothingHair);
             }
             if (selectedCategory == 2) {
+                if (selectedClothingAccessory == 1) {
+                    selectedClothingAccessory = 3;
+                } 
+                else  {
+                    selectedClothingAccessory--;
+                }
+                UpdateAccessoryIndicator(selectedClothingAccessory);
+            }
+            if (selectedCategory == 3) {
+                if (selectedClothingShoe == 1) {
+                    selectedClothingShoe = 3;
+                } 
+                else  {
+                    selectedClothingShoe--;
+                }
+                UpdateShoeIndicator(selectedClothingShoe);
+            }
+            if (selectedCategory == 4) {
                 if (selectedClothingTop == 1) {
                     selectedClothingTop = 3;
                 } 
@@ -149,7 +179,8 @@ public class ClothingSelection : MonoBehaviour
                 }
                 UpdateTopIndicator(selectedClothingTop);
             }
-            if (selectedCategory == 3) {
+
+            if (selectedCategory == 5) {
                 if (selectedClothingBottom == 1) {
                     selectedClothingBottom = 3;
                 } 
@@ -157,15 +188,6 @@ public class ClothingSelection : MonoBehaviour
                     selectedClothingBottom--;
                 }
                 UpdateBottomIndicator(selectedClothingBottom);
-            }
-            if (selectedCategory == 4) {
-                if (selectedClothingShoe == 1) {
-                    selectedClothingShoe = 3;
-                } 
-                else  {
-                    selectedClothingShoe--;
-                }
-                UpdateShoeIndicator(selectedClothingShoe);
             }
             
             //if (selectedCategory == 6) {
@@ -179,91 +201,98 @@ public class ClothingSelection : MonoBehaviour
     private void UpdateHairIndicator(int x) {
         if (x == 1) {
             hairBox.transform.position = new Vector3(-0.76f, 3.53f, 0f);
-            bonusHairText.text = "Hitting notes is a little easier.";
         }
 
         if (x == 2) {
             hairBox.transform.position = new Vector3(0.5f, 3.53f, 0f);
-            bonusHairText.text = "Score increased by 20%.";
         }
 
         if (x == 3) {
             hairBox.transform.position = new Vector3(1.7f, 3.54f, 0f);
-            bonusHairText.text = "Start with more coolness.";
         }
         
     }
 
-    private void UpdateTopIndicator(int x) {
+    private void UpdateAccessoryIndicator(int x) {
         if (x == 1) {
-            topBox.transform.position = new Vector3(-0.88f, 1.39f, 0f);
-            bonusTopText.text = "Hitting notes is a little easier.";
+            accessoryBox.transform.position = new Vector3(-0.82f, 1.44f, 0f);
         }
 
         if (x == 2) {
-            topBox.transform.position = new Vector3(0.39f, 1.36f, 0f);
-            bonusTopText.text = "Score increased by 20%.";
+            accessoryBox.transform.position = new Vector3(0.53f, 1.42f, 0f);
         }
 
         if (x == 3) {
-            topBox.transform.position = new Vector3(1.69f, 1.39f, 0f);
-            bonusTopText.text = "Start with more coolness.";
+            accessoryBox.transform.position = new Vector3(1.84f, 1.42f, 0f);
         }
-    }
-
-    private void UpdateBottomIndicator(int x) {
-        if (x == 1) {
-            bottomBox.transform.position = new Vector3(3.6f, 3.03f, 0f);
-            bonusBottomText.text = "Hitting notes is a little easier.";
-        }
-
-        if (x == 2) {
-            bottomBox.transform.position = new Vector3(5.58f, 3f, 0f);
-            bonusBottomText.text = "Score increased by 20%.";
-        }
-
-        if (x == 3) {
-            bottomBox.transform.position = new Vector3(7.41f, 2.97f, 0f);
-            bonusBottomText.text = "Start with more coolness.";
-        }
+        
     }
 
     private void UpdateShoeIndicator(int x) {
         if (x == 1) {
             shoeBox.transform.position = new Vector3(-0.83f, -0.77f, 0f);
-            bonusShoeText.text = "Hitting notes is a little easier.";
         }
 
         if (x == 2) {
             shoeBox.transform.position = new Vector3(0.44f, -0.79f, 0f);
-            bonusShoeText.text = "Score increased by 20%.";
         }
 
         if (x == 3) {
             shoeBox.transform.position = new Vector3(1.71f, -0.76f, 0f);
-            bonusShoeText.text = "Start with more coolness.";
+        }
+    }
+
+    private void UpdateTopIndicator(int x) {
+        if (x == 1) {
+            topBox.transform.position = new Vector3(3.8f, 3f, 0f);
+        }
+
+        if (x == 2) {
+            topBox.transform.position = new Vector3(5.52f, 3f, 0f);
+        }
+
+        if (x == 3) {
+            topBox.transform.position = new Vector3(7.28f, 3f, 0f);
+        }
+    }
+
+    private void UpdateBottomIndicator(int x) {
+        if (x == 1) {
+            bottomBox.transform.position = new Vector3(3.71f, 0f, 0f);
+        }
+
+        if (x == 2) {
+            bottomBox.transform.position = new Vector3(5.62f, 0f, 0f);
+        }
+
+        if (x == 3) {
+            bottomBox.transform.position = new Vector3(7.41f, 0f, 0f);
         }
     }
 
     private void UpdateCategoryIndicator(int x) {
 
-        if (x == 1) {
+        if (x == 1) {   //hair
             categoryBox.transform.position = new Vector3(0.5f, 3.53f, 0f);
         }
 
-        if (x == 2) {
-            categoryBox.transform.position = new Vector3(0.39f, 1.36f, 0f);
+        if (x == 2) {   //accessory
+            categoryBox.transform.position = new Vector3(0.53f, 1.42f, 0f);
         }
 
-        if (x == 3) {
-            categoryBox.transform.position = new Vector3(5.58f, 3f, 0f);
-        }
-
-        if (x == 4) {
+        if (x == 3) {   //shoe
             categoryBox.transform.position = new Vector3(0.44f, -0.79f, 0f);
         }
 
-        if (x == 5) {
+        if (x == 4) {   //top
+            categoryBox.transform.position = new Vector3(5.52f, 3f, 0f);
+        }
+
+        if (x == 5) {   //bottom
+            categoryBox.transform.position = new Vector3(5.62f, 0f, 0f);
+        }
+
+        if (x == 6) {   //start btn
             categoryBox.transform.position = new Vector3(4f, -3.8f, 0f);
             startText.text = "→ to start!";
         }
