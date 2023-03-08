2bd83492a294cc282b1ad237d9b9a9b993f4f02d
msett <msett@my.yorku.ca>
2023-03-08 16:41:38 -0500

    0.65b
    Left/right char animate in & out of focus properly dependent on speaking text. Still need to animate transitions in & out, as well as character switches.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,32 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
     public TMPro.TextMeshPro nameText;
-    public TMPro.TextMeshPro dialogueText;
+    public TMPro.TextMeshPro dialogueText;      //altering the textfields in-game.
 
-    private bool conversationStarted = false;
+    private bool conversationStarted = false;   //boolean trigger to see if conversation has started.
+
     private Queue<string> sentences;
-    private Queue<string> namesList;
-    public KeyCode nextButton;
+    private Queue<string> namesList;    //one queue for the text, another queue for the names of who's speaking
 
-    public GameObject dialogueToTrigger;
+    public KeyCode nextButton;          //key for navigating through the dialogue
+
+    public GameObject characterOnLeft;
+    public GameObject characterOnRight; //the gameobjects control the images (I hope (untested))
+    public Animator animateLeft;
+    public Animator animateRight;       //the animators control the fade-in/fade-out
 
     void Start()
     {   
         sentences = new Queue<string>();
-        namesList = new Queue<string>();
+        namesList = new Queue<string>();        //initializes the queues. queues are FIFO
+        animateLeft.SetBool("mainCharIsSpeaking", false);
+        animateRight.SetBool("rightCharIsSpeaking", false);     //initializes both characters to not be speaking
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(nextButton)) {
+        if (Input.GetKeyDown(nextButton)) { //if conversation started, cycle through the sentences. if not, begin the dialogue.
             switch (conversationStarted) {
                 case false:
                     DialogueTrigger.Instance.TriggerDialogue();
-                    //dialogueToTrigger.GetComponent<DialogueTrigger>().Instance.TriggerDialogue();
                     conversationStarted = true;
                     break;
                 case true:
@@ -39,32 +46,41 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue) 
     {
-        sentences.Clear();
+        sentences.Clear();  //clears any existing dialogue and names
         namesList.Clear();
 
-        foreach (string sentence in dialogue.sentences) 
+        foreach (string sentence in dialogue.sentences) //shoves all sentences in the queue
         {
             sentences.Enqueue(sentence);
         }
 
-        foreach (string name in dialogue.names) 
+        foreach (string name in dialogue.names)         //shoves all names in the queue
         {
             namesList.Enqueue(name);
         }
 
-        DisplayNextSentence();
+        DisplayNextSentence();  //it only does this once. gets out of the startdialogue by initializing the dialogue stream.
     }
 
     public void DisplayNextSentence() {
-        if (sentences.Count == 0) {
+        if (sentences.Count == 0) {     //if there are no sentences left, end the dialogue
             EndDialogue();
             return;
         }
 
-        string sentence = sentences.Dequeue();
-        string name = namesList.Dequeue();
-        StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence));
+        string sentence = sentences.Dequeue();  //offload the next sentences and name in the queue
+        string name = namesList.Dequeue();      
+
+        if (name == "Jassmea") {                //this logic is under the assumption that the main character is always on the left.
+            animateLeft.SetBool("mainCharIsSpeaking", true);    
+            animateRight.SetBool("rightCharIsSpeaking", false);
+        } else {                                //if the main char is speaking, highlight them. if not, make them out of focus. vice-versa for npcs.
+            animateLeft.SetBool("mainCharIsSpeaking", false);   
+            animateRight.SetBool("rightCharIsSpeaking", true);
+        }
+
+        StopAllCoroutines();    //<- when the next sentence is loaded when the next one isn't finished yet.
+        StartCoroutine(TypeSentence(sentence)); //to gradually load in the next sentence, letter by letter, check the TypeSentence IEnumerator.
         nameText.text = name;
     }
 
@@ -78,6 +94,7 @@ public class DialogueManager : MonoBehaviour
     }
 
     void EndDialogue() {
+        //todo, for scene transitions.
         Debug.Log("End of conversation.");
     }
 }
